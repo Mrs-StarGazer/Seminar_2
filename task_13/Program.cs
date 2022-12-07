31858e2a785bc06573c2588ed5620c9f476881ce
Maria <m.shklyaeva96@gmail.com>
2022-12-07 15:09:44 +0500

    add task 13

---
+++ task_13/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("введите число");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number < 0)
+{
+    number = -number;
+}
+if (number < 100)
+{
+    Console.WriteLine("третей цифры нет");
+}
+else
+{
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    int thirdDigit = number % 10;
+    Console.WriteLine(thirdDigit);
+}
