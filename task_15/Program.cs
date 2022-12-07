7cd2843648d711c35ec0fc27f69f2c204f120bfe
Maria <m.shklyaeva96@gmail.com>
2022-12-07 23:46:12 +0500

    add task 15

---
+++ task_15/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("введите число");
+int dayWeek = int.Parse(Console.ReadLine()!);
+
+if (dayWeek==1)
+{
+    Console.WriteLine ("нет");
+}
+if (dayWeek==2)
+{
+    Console.WriteLine ("нет");
+}
+if (dayWeek==3)
+{
+    Console.WriteLine ("нет");
+}
+if (dayWeek==4)
+{
+    Console.WriteLine ("нет");
+}
+if (dayWeek==5)
+{
+    Console.WriteLine ("нет");
+}
+if (dayWeek==6)
+{
+    Console.WriteLine ("выходной");
+}
+if (dayWeek==7)
+{
+    Console.WriteLine ("выходной");
+}
